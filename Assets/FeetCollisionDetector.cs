2bb48f5249f701085520063a79e9ea6ba9ae23e3
David Říha <davedave0008@gmail.com>
2025-04-08 15:16:36 +0200

    Added ground detecting collider to character

---
+++ Assets/FeetCollisionDetector.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FeetCollisionDetector : MonoBehaviour
+{
+    public UnityEvent feetTriggerStay;
+    public UnityEvent feetTriggerExit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+        Debug.Log("Trigger feet stay!");
+        feetTriggerStay.Invoke();
+    }
+
+	private void OnTriggerExit(Collider other)
+	{
+        Debug.Log("Trigger feet exit!");
+        feetTriggerExit.Invoke();
+	}
+}
