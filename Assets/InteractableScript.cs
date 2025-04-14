6c84c27cad5bb30c855e5c1c9c676780e1c244e2
David Říha <davedave0008@gmail.com>
2025-04-14 15:52:24 +0200

    Label now tweens on interact

--- Assets/InteractableScript.cs
+++ Assets/InteractableScript.cs
@@ -1,21 +1,59 @@
+using DG.Tweening;
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class InteractableScript : MonoBehaviour
 {
-    Collider proximity;
+	public UnityEvent OnInteract;
+    
+	
+	Collider proximity;
 	GameObject label;
 	bool playerInProximity;
 
-	public UnityEvent OnInteract;
+	InputAction interactAction;
+
+	float scaleTweenRatio = 1.3f;
+	float scaleTweenDuration = 0.3f;
+
 
 	private void Start()
 	{
 		proximity = GetComponent<Collider>();
 		label = transform.Find("Label").gameObject;
 		label.SetActive(false);
+
+		interactAction = InputSystem.actions.FindAction("Interact");
+
+		OnInteract.AddListener(TweenLabel);
 	}
+
+
+	private void Update()
+	{
+		if (playerInProximity && interactAction.WasPressedThisFrame())
+			OnInteract.Invoke();
+
+
+	}
+
+
+	private void TweenLabel()
+	{
+		label.transform.DOComplete();
+		Vector3 origScale = label.transform.localScale;
+		Sequence seq = DOTween.Sequence();
+		seq.Append(label.transform.DOScale(origScale * scaleTweenRatio, scaleTweenDuration/2));
+		seq.Append(label.transform.DOScale(origScale, scaleTweenDuration / 2));
+
+		//Debug.Log("Tweening label");
+		seq.Play();
+	}
+
+
 
 	private void OnTriggerEnter(Collider other)
 	{
