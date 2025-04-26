9e0d7663b8465268ff91da4b3deaf29608072ff6
David Říha <davedave0008@gmail.com>
2025-04-26 17:01:04 +0200

    Tried to fix freezing Global Scale

--- Assets/SetGlobalScale.cs
+++ Assets/SetGlobalScale.cs
@@ -6,9 +6,14 @@ public class SetGlobalScale : MonoBehaviour
 {
 	public Vector3 fixedScale = new Vector3(1, 1, 1);
 	// Update is called once per frame
-	void Start()
+	void OnEnable()
 	{
+		//Debug.Log("Global scale = " + transform.lossyScale.ToString());
 		if (transform.lossyScale != fixedScale)
-			transform.localScale = new Vector3(fixedScale.x / transform.lossyScale.x, fixedScale.y / transform.lossyScale.y, fixedScale.z / transform.lossyScale.z);
+		{
+			Vector3 globalIfLocal1 = new Vector3(transform.lossyScale.x/transform.localScale.x, transform.lossyScale.y / transform.localScale.y, transform.lossyScale.z / transform.localScale.z);
+			//Debug.Log("Setting local scale to :" + (fixedScale.x / globalIfLocal1.x) + " , " + (fixedScale.y / globalIfLocal1.y) + " , " + (fixedScale.z / globalIfLocal1.z));
+			transform.localScale = new Vector3(fixedScale.x / globalIfLocal1.x, fixedScale.y / globalIfLocal1.y, fixedScale.z / globalIfLocal1.z);
+		}
 	}
 }
