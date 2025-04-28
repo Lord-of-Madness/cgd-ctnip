4f34347b13f2320795cfd1cc89fd4cb09fa6393a
David Říha <davedave0008@gmail.com>
2025-04-28 16:00:28 +0200

    Introduced Camera Flash to Beth

---
+++ Assets/CameraFlashScript.cs
@@ -0,0 +1,60 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.InputSystem;
+
+public class CameraFlashScript : MonoBehaviour
+{
+    [SerializeField]
+    float exposureTime = 0.5f;
+    [SerializeField]
+    float flashIntesity = 400;
+    [SerializeField]
+    float flashRange = 20;
+    [SerializeField]
+    Ease tweenEase = Ease.Linear;
+
+    [Header("References")]
+    [SerializeField]
+    Transform rotationTransformRef;
+
+	private InputActionsGen inputActions;
+
+	Light flashLight;
+
+    Tween flashTween;
+    bool flashing = false;
+
+	private void Awake()
+	{
+		inputActions = new();
+		inputActions.Player.Enable();
+	}
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        flashLight = GetComponent<Light>();
+		inputActions.Player.CameraFlash.performed += (ctx) => { Flash(); };
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        transform.rotation = rotationTransformRef.rotation;
+    }
+
+
+    public void Flash()
+    {
+        if (flashing || (flashTween != null && flashTween.active)) return;
+
+
+        flashLight.intensity = flashIntesity;
+        flashLight.range = flashRange;
+        flashTween = flashLight.DOIntensity(0, exposureTime).SetEase(tweenEase);
+        flashing = true;
+        flashTween.OnComplete(() => flashing = false);
+    }
+}
