7d1a78983bbdad43fd515036d630635bd499a16e
David Říha <davedave0008@gmail.com>
2025-05-05 16:52:16 +0200

    Started working on enemy attacking

---
+++ Assets/EnemyAttackHitScript.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemyAttackHitScript : MonoBehaviour
+{
+    SphereCollider sphereCol;
+    BoxCollider boxCol;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sphereCol = GetComponent<SphereCollider>();
+        boxCol = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<Collider> GetAllObjectsInAttackArea()
+    {
+        Collider[] colsInBox = Physics.OverlapBox(boxCol.center, boxCol.size / 2);
+        Collider[] colsInSphere = Physics.OverlapSphere(sphereCol.center, sphereCol.radius);
+
+        List<Collider> colsInAttackArea = new();
+        foreach (Collider col in colsInSphere) {
+            bool duplicate = false;
+            foreach (Collider col2 in colsInBox)
+                if (col2 == col) { duplicate = true; break; }
+            if (!duplicate) colsInAttackArea.Add(col);
+        }
+
+        return colsInAttackArea;
+
+    }
+}
--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -2,21 +2,33 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+
+    AITarget aiTargetScript;
+
     int hp = 100;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        aiTargetScript = GetComponent<AITarget>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetAITargetToCloserChar();
     }
 
     public void GetHit()
     {
         Debug.Log("I GOT HIT!! Only " +  hp + "left"); 
+    }
+
+    void SetAITargetToCloserChar()
+    {
+        Transform target = GameManager.Instance.bethPC.transform;
+        if ((GameManager.Instance.erikPC.transform.position - transform.position).magnitude <= (GameManager.Instance.bethPC.transform.position - transform.position).magnitude) { 
+            target = GameManager.Instance.erikPC.transform;
+        }
+        aiTargetScript.target = target;
     }
 }
