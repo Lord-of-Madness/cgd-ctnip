d0c966b20cabd8c6c191518ec3c55e4fae9e998f
malyjir <zemekoulak@gmail.com>
2025-05-06 02:48:11 +0200

    Dialogue converted to tree structure. Added serialization for dialogs.

--- Assets/DialogueGiver.cs
+++ Assets/DialogueGiver.cs
@@ -9,19 +9,28 @@ public class DialogueGiver : MonoBehaviour
     string charName;
     [SerializeField]
     Sprite sprite;
+    [SerializeField] string DialogueJSON ="";
 
     private readonly List<DialogueLine> diaLines = new();
     bool shown = false;
 
+    public bool DEBUGGENERATEJSON = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (DialogueJSON != "" && (lines==null || lines.Count==0))
+        {
+            lines = JsonUtility.FromJson<List<string>>(DialogueJSON);
+        }
         initDialogueLines();
     }
-
+    /// <summary>
+    /// Only for testing purposes
+    /// </summary>
     void initDialogueLines()
     {
-        for (int i = 0; i < lines.Count; i++)
+        for (int i = 0; i < lines.Count; i++)//Testing explicit speakers
         {
             if (i % 2 == 0)
             {
@@ -42,10 +51,17 @@ public class DialogueGiver : MonoBehaviour
     {
         if (!shown)
         {
-            Dialogue.Instance.ShowCharacterWithText(diaLines,() => {
+            DialogueTreeNode dialogueRoot = DialogueTreeNode.BuildSimpleTree(diaLines);
+            if (DEBUGGENERATEJSON)
+            {
+                dialogueRoot.SerializeTree("IHaveNoClueWhatThisWillDo.json");//Application.dataPath
+            }
+
+            Dialogue.Instance.ShowCharacterWithText(dialogueRoot /*,() => {
                 shown = true;
                 GameManager.Instance.ActivePlayer.playerData.Documents.Add(new("TestDocument", "BEHOLD THIS IS THE TEXT DOCUMENT OF DOOM!"));
-            });//Horribly se to podìlá kdyby nìco zabilo NPCèko bìhem hovoru.
+            }*/);//Horribly se to podìlá kdyby nìco zabilo NPCèko bìhem hovoru.
         }
     }
+    
 }
--- Assets/UIScripts/Dialogue.cs
+++ Assets/UIScripts/Dialogue.cs
@@ -4,7 +4,131 @@ using System.Collections.Generic;
 using DG.Tweening;
 using Unity.VisualScripting;
 using System;
+using NUnit.Framework.Interfaces;
+using System.Linq;
 
+
+public class DialogueTreeNode
+{
+    public List<DialogueTreeNode> children = new();
+    public DialogueLine Line { get; set; }
+    public bool IsLeaf => children.Count == 0;
+    public bool IsChoice => children.Count > 1;
+    public Action callback;
+    private const int FUNNY_NUMBER = 69420; //Haha funny. Once you stop being utterly hilarious you can replace it with -1 or something.
+    private int SerializationID = FUNNY_NUMBER;
+    public DialogueTreeNode(DialogueLine line)
+    {
+        Line = line;
+    }
+
+    internal void AddChild(DialogueTreeNode dialogueTreeNode)
+    {
+        children.Add(dialogueTreeNode);
+    }
+    public static DialogueTreeNode BuildSimpleTree(List<DialogueLine> lines)
+    {
+        DialogueTreeNode root = new(lines[0]);
+        DialogueTreeNode workingNode = root;
+        for (int i = 1; i < lines.Count; i++)
+        {
+            DialogueTreeNode dialogueTreeNode = new(lines[i]);
+            workingNode.AddChild(dialogueTreeNode);
+            workingNode = dialogueTreeNode;
+        }
+        return root;
+    }
+    public DialogueTreeNode GetFirstLeaf()
+    {
+        if (IsLeaf) return this;
+        else
+        {
+            foreach (var child in children)
+            {
+                if (child.IsLeaf) return child;
+                else return child.GetFirstLeaf();
+            }
+        }
+        return null;
+    }
+    public DialogueTreeNode DeserializeTree(string JSONPATH )
+    {
+        List<DialogueNodeJSON> list = JsonUtility.FromJson<DialogueWrapper>(Resources.Load<TextAsset>(JSONPATH).text).nodes;
+        Dictionary<int, DialogueTreeNode> nodeDict = new();
+        foreach (var node in list)
+        {
+            nodeDict.Add(node.id, new(new(node.Text,node.Who,Resources.Load<Sprite>($"CharacterPortraits/{node.Who}"),node.Hex) ));
+        }
+        DialogueTreeNode root = nodeDict.ContainsKey(0) ? nodeDict[0] : null;
+        if(root == null) Debug.LogError($"No root node found in {JSONPATH}");
+        foreach (var node in list)
+        {
+            DialogueTreeNode currentNode = nodeDict[node.id];
+            foreach (var childId in node.children)
+            {
+                if (nodeDict.ContainsKey(childId))
+                {
+                    currentNode.AddChild(nodeDict[childId]);
+                }
+                else
+                {
+                    Debug.LogError($"Child node {childId} not found for parent {node.id}");
+                }
+            }
+
+        }
+        return root;
+    }
+    int SerializeNodeRecursion(Dictionary<int,DialogueNodeJSON> nodeList)
+    {
+        if (SerializationID != FUNNY_NUMBER) return SerializationID;
+        DialogueNodeJSON nodeJSON = new()
+        {
+            id = nodeList.Count,
+            Text = Line.Text,
+            Who = Line.Who,
+            Hex = Line.Hex,
+            children = new List<int>()
+        };
+        SerializationID = nodeJSON.id;
+        nodeList.Add(nodeJSON.id, nodeJSON);
+        foreach (var child in children)
+        {
+            nodeJSON.children.Add(child.SerializeNodeRecursion(nodeList));
+        }
+
+        return SerializationID;
+    }
+    public void SerializeTree(string path)
+    {
+        Dictionary<int, DialogueNodeJSON> nodeList = new();
+        SerializeNodeRecursion(nodeList);
+        //WARNING/TODO: the entire tree is dirty after this and cannot be serialized again - Need to set the SerializationID back to FUNNY_NUMBER
+        Debug.Log(nodeList.Count);
+        string json = JsonUtility.ToJson(new DialogueWrapper(nodeList.Values.ToList()));
+        Debug.Log(json);
+        System.IO.File.WriteAllText(path, json);
+    }
+}
+[System.Serializable]
+public class DialogueWrapper
+{
+    public List<DialogueNodeJSON> nodes;
+    public DialogueWrapper(List<DialogueNodeJSON> nodes)
+    {
+        this.nodes = nodes;
+    }
+}
+[System.Serializable]
+public class DialogueNodeJSON
+{
+    public int id;
+    public string Text;
+    public string Who;
+    //public string SpritePath { get; set; } //Lets just use Who and a concrete folder in Resources.
+    public string Hex;
+    public List<int> children;
+}
 
 public class DialogueLine
 {
@@ -56,14 +180,13 @@ public class Dialogue : MonoBehaviour
 {
     [SerializeField]Image CharacterImage;
     [SerializeField] GameObject dialogueBox;
-    [SerializeField] GameObject dialogTextLinePrefab;
-    //[SerializeField] Text SpeakerName;
+    [SerializeField] Text dialogTextLinePrefab;
+    [SerializeField] Button dialogOptButtonPrefab;
     Tween textween;
 
-    Queue<DialogueLine> lines = new();
+    DialogueTreeNode lines;
     [SerializeField] float textSpeed = 0.3f;
     public static Dialogue Instance { get; private set; }
-    Action callback = null;
     private void Awake()
     {
         Instance = this;
@@ -81,16 +204,15 @@ public class Dialogue : MonoBehaviour
         CharacterImage.sprite = line.Sprite;
         if (line.Sprite==null)Debug.LogWarning($"Speaker: {line.Who} has no SpritePortrait");
         if (textween != null) FinishTween();
-        Text dialogTextLine = Instantiate(dialogTextLinePrefab, dialogueBox.transform).GetComponent<Text>();
+        Text dialogTextLine = Instantiate(dialogTextLinePrefab, dialogueBox.transform);
         dialogTextLine.text = "";//If we clean up the prefab this can be removed.
         textween = dialogTextLine.DOText(line.SpeakerAnnotation()+line.Text, textSpeed, true, ScrambleMode.None).OnComplete(() => FinishTween());
     }
-    public void ShowCharacterWithText(List<DialogueLine> lines,Action callback=null)
+    public void ShowCharacterWithText(DialogueTreeNode lines)
     {
         Show();
         Utilities.PurgeChildren(dialogueBox.transform);
-        this.lines = new(lines);
-        this.callback = callback;
+        this.lines = lines;
         NextLine();
     }
     private void SkipText()
@@ -100,13 +222,22 @@ public class Dialogue : MonoBehaviour
     }
     private void NextLine()
     {
-        if (lines.Count > 0)
+        if (!lines.IsLeaf)
         {
-            ShowCharacterWithText(lines.Dequeue());
+            if (lines.IsChoice)
+            {
+                Debug.Log("TODOChoice");//TODO
+            }
+            else {
+                ShowCharacterWithText(lines.Line);
+                lines.callback?.Invoke();
+                lines = lines.children[0];
+            }
         }
-        else {
-            callback?.Invoke();
-            Hide(); }
+        else
+        {
+            Hide();
+        }
     }
     void FinishTween()
     {
@@ -121,7 +252,7 @@ public class Dialogue : MonoBehaviour
     void Hide()
     {
         FinishTween();
-        lines.Clear();
+        lines=null;
         GameManager.Instance.inputActions.Player.Enable();
         GameManager.Instance.inputActions.Dialogue.Disable();
         gameObject.SetActive(false);
