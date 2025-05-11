3bf30176fe4fd280187f2d23ae0bff4afdf88e90
David Říha <davedave0008@gmail.com>
2025-05-11 23:41:38 +0200

    Mansion disables char swap

--- Assets/MansionSceneManager.cs
+++ Assets/MansionSceneManager.cs
@@ -5,6 +5,12 @@ public class MansionSceneManager : MonoBehaviour
 	public static bool KeyPickedUp = false;	
 	public static bool EnemiesReleased = false;
 
+
+	private void Start()
+	{
+		GameManager.Instance.inputActions.Player.SwapCharacters.Disable();
+	}
+
 	public static void PickUpKey()
 	{
 		KeyPickedUp = true;
