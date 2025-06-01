7a4258c53647d4547420d4a0ad1cadf77a6fd28c
David Říha <davedave0008@gmail.com>
2025-06-01 16:21:18 +0200

    Fixed enemy dying when loading

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -249,11 +249,13 @@ public class EnemyScript : MonoBehaviour, SaveSystem.ISaveable
 
 	public void Load(SaveSystem.AllSavedData data)
 	{
+        //TODO: Enemy resurrection when loading from dead to alive
         SaveSystem.EnemyData myData = data.enemyData[Utilities.GetFullPathName(gameObject)];
         if (myData.following) ResumeFollowingTarget();
         else StopFollowingTarget();
 
         hp = myData.hp;
+        bodyAnimator.SetInteger(GlobalConstants.animHpID, hp);
         transform.position = myData.pos.GetVector3();
 
 	}
