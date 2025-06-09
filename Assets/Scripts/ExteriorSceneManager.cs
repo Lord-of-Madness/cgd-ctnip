c26116dfa73b6efde7ab33c2953913427a48b3d6
malyjir <zemekoulak@gmail.com>
2025-06-09 14:22:10 +0200

    Adding Exterior sound

--- Assets/Scripts/ExteriorSceneManager.cs
+++ Assets/Scripts/ExteriorSceneManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class ExteriorSceneManager : MonoBehaviour
@@ -7,9 +8,13 @@ public class ExteriorSceneManager : MonoBehaviour
     [SerializeField] TextAsset Outside;
     private DialogueTreeNode MissionBriefDT;
     private DialogueTreeNode OutsideDT;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip ambientMusic;
     void Start()
     {
         MissionBriefDT = DialogueTreeNode.DeserializeTree(MissionBrief);
+        audioSource.clip = ambientMusic;
+        audioSource.Play();
         OutsideDT = DialogueTreeNode.DeserializeTree(Outside);
         StartCoroutine(Utilities.CallAfterSomeTime(() => Dialogue.Instance.ShowCharacterWithText(MissionBriefDT),1f));
     }
