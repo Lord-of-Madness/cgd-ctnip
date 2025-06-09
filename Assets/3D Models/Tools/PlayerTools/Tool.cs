87dac6b72cc4e52ac06ccf835a51b9bb946ff7e3
Barča <117365855+Talirek123@users.noreply.github.com>
2025-06-09 20:21:13 +0200

    Huge Cleanup zbytek

---
+++ Assets/3D Models/Tools/PlayerTools/Tool.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    public string toolName;
+    public Sprite toolIcon;
+    public int maxLoadedAmmo;
+    public int reloadBatch;
+    public float reloadTime;
+    public float actionTime;
+    public AudioClip reloadSound;
+    public AudioClip fireSound;
+    public bool hasToAim;
+    public bool infinteReloads;
+}
