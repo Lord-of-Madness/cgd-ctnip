bf4249a0b2cc6de9e6506953fe71b4d509a853ed
malyjir <zemekoulak@gmail.com>
2025-06-15 17:04:52 +0200

    Erik scene is now story worthy

--- Assets/ErikSceneManager.cs
+++ Assets/ErikSceneManager.cs
@@ -4,9 +4,11 @@ public class ErikSceneManager : MonoBehaviour
 {
     [SerializeField] GameObject Lights;
     [SerializeField] TextAsset BloodInspectionJSON;
+    [SerializeField] SceneTransition sceneTransition;
     public void BloodInspection()
     {
         Lights.SetActive(false);
         Dialogue.Instance.ShowCharacterWithText(DialogueTreeNode.DeserializeTree(BloodInspectionJSON));
+        sceneTransition.Enabled = true;
     }
 }
--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,8 +6,9 @@ public class SceneTransition : MonoBehaviour
 	string loadedSceneName = "GramofonScene";
 
 	bool readyToTrans = false;
+	public bool Enabled = true;
 
-	private void Start()
+    private void Start()
 	{
 		//Wait for 2 secs after scene load beforea transitioning back
 		StartCoroutine(Utilities.CallAfterSomeTime(() => readyToTrans = true, 2));//Víš, že staèí použít Invoke(nameof(fce),èas) right?
@@ -16,7 +17,7 @@ public class SceneTransition : MonoBehaviour
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (!readyToTrans) return;
+		if (!readyToTrans || !Enabled) return;
 		//if (other == null || GameManager.Instance.ActivePlayer == null) return; //probably popped in between loading
 		if (Utilities.ActivePlayerCheck(other.gameObject))
 		{
